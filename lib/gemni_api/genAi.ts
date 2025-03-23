000b2098c2ba4b30b70f652814c27780c351a989
nitin <nitinrana01125532553@gmail.com>
2025-03-23 12:05:10 +0530

    changes

--- lib/gemni_api/genAi.ts
+++ lib/gemni_api/genAi.ts
@@ -1,6 +1,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.GEMNI_API_KEY ?? "");
+const apiKey =
+  process.env.GEMINI_API_KEY || process.env.NEXT_PUBLIC_GEMINI_API_KEY || "";
+const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash",
 });
